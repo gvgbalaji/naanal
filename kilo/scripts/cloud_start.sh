581fc5547fc3e861db39a6c5863f0ff76a91c5e4
selvac <selvac@naanal.in>
2015-10-09 19:47:39 +0530

    start
    start

---
+++ kilo/scripts/cloud_start.sh
@@ -0,0 +1,25 @@
+service networking restart;
+
+service mysql restart;
+
+service keystone restart;
+
+service glance-api restart;
+service glance-registry restart;
+
+service nova-api restart ;
+service nova-cert restart; 
+service nova-consoleauth restart ;
+service nova-scheduler restart;
+service nova-conductor restart; 
+service nova-novncproxy restart; 
+service nova-compute restart; 
+service nova-console restart
+
+service neutron-server restart; 
+service neutron-plugin-openvswitch-agent restart;
+service neutron-metadata-agent restart; 
+service neutron-dhcp-agent restart; 
+service neutron-l3-agent restart;
+
+
