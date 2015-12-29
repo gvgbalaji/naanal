11265e89f7f429844ecb321ace17ee44d3550277
maniram477 <maniram477@gmail.com>
2015-12-29 11:52:59 +0530

    Bug Fixed HA

--- cloud/solutions/ha/complete_py.py
+++ cloud/solutions/ha/complete_py.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
-controller_ip="192.168.30.168"
-target_host = "naanal-laptop1"
-#lan_net = "55f220d8-f0ce-402c-b510-6795c85f2675"
-
+controller_ip="192.168.1.230"
+target_host = "naanal-cloud"
 
 import time
 import MySQLdb
@@ -23,7 +21,8 @@ def active_hosts():
     return [host for host in nova.services.list(binary="nova-compute") if host.state == 'up']
 
 def host_delete(host_obj):
-    nova.services.delete(host_obj[0].id)
+    print host_obj.host
+    nova.services.delete(host_obj.id)
 
 ### Instance Functions ###
     
@@ -34,92 +33,125 @@ def list_instances(host_name):
 def parse_inst_details(instance_obj):
     info = instance_obj._info
     inst_id = info.get('id')
-    name = info.get('name')
-    image_id = info.get('image')['id']
-    flavor_id = info.get('flavor')['id']
-    key_name = info.get('key_name')
-    fixed_ip = info.get('addresses')['lan-net'][0]['addr']
-    flt_ip = info.get('addresses')['lan-net'][1]['addr']
-    volume_id = info.get('os-extended-volumes:volumes_attached')[0]['id']
-    return dict([(key,str(eval(key))) for key in ('name','image_id','flavor_id','key_name','fixed_ip','volume_id','inst_id','flt_ip')])
+    name = str(info.get('name'))
+    image_id = str(info.get('image')['id'])
+    flavor_id = str(info.get('flavor')['id'])
+    key_name = str(info.get('key_name'))
+    vol_ids = info.get('os-extended-volumes:volumes_attached')
+    fixed_ip_list = []
+    flt_ip_list = []
+    
+    vol_dict = {}
+
+    for ip in info.get('addresses')['lan-net']:
+        if ip['OS-EXT-IPS:type'] == 'floating':
+            flt_ip_list.append(str(ip['addr']))
+        elif ip['OS-EXT-IPS:type'] == 'fixed':
+            fixed_ip_list.append(str(ip['addr']))
+    
+    for vol in vol_ids:
+        mount = cinder.volumes.get(vol['id']).attachments[0]['device']
+        vol_dict[vol['id']] = mount
+
+    return dict([(key,eval(key)) for key in ('name','image_id','flavor_id','key_name','fixed_ip_list','flt_ip_list','vol_dict','inst_id')])
 
 
-def create_instance_mysql(name,volume_id,flt_ip):
+def delete_instance_mysql(uuid,volume_id,host_name,flt_ip_list):
+    print "Fake Deletion"
+    volume_detach(volume_id)
+    
+    tmp_ins_obj = nova.servers.get(uuid)
+    flt_ip_detach(tmp_ins_obj,flt_ip_list)
+    
+    delete_q = "update nova.instances set deleted=1, deleted_at=now() where uuid='{0}' and  host = '{1}';".format(uuid,host_name)
+    print(delete_q+"\n")
+    delete_cur = con.cursor()
+    delete_cur.execute(delete_q)
+    delete_cur.close()
+    con.commit()
+
+
+
+def create_instance_mysql(name,volume_id,flt_ip_list):
+    print "Fake Creation"
     # By using similiar Naming Pattern We should fetch and Turn on Standby Instance(it's uuid is unknown) Eg:(Active: test; Standby: test-ha)
     name = name + '-ha'
-    uuid_q = "select uuid nova.instances where display_name='{0}' and host='{1}';".format(name,target_host)
+
+    uuid_q = "select uuid from nova.instances where display_name='{0}' and host='{1}';".format(name,target_host)
     print(uuid_q+"\n")
     uuid_cur = con.cursor()
     uuid_cur.execute(uuid_q)
     uuid = uuid_cur.fetchone()
+    uuid_cur.close()
+    con.commit()
 
-    undelete_q = "update nova.instances set deleted=0 where uuid='{0}';".format(uuid)
+    uuid = uuid[0]
+    undelete_q = "update nova.instances set deleted=0,deleted_at=NULL where uuid='{0}';".format(uuid)
     print(undelete_q+"\n")
-    #undelete_cur = con.cursor()
-    #undelete_cur.execute(undelete_q)
+    undelete_cur = con.cursor()
+    undelete_cur.execute(undelete_q)
+    undelete_cur.close()
+    con.commit()
 
     tmp_ins_obj = nova.servers.get(uuid)
-    print tmp_ins_obj.name
-    #flt_ip_attach(tmp_ins_obj,flt_ip)
-    #volume_attach(volume_id,uuid)
-    #tmp_ins_obj.start()
+    flt_ip_attach(tmp_ins_obj,flt_ip_list)
+    volume_attach(volume_id,uuid)
+    tmp_ins_obj.start()
 
-
-def delete_instance_mysql(uuid,volume_id,host_name):
-    #volume_detach(volume_id)
-    
-    tmp_ins_obj = nova.servers.get(uuid)
-    print tmp_ins_obj.name
-    #flt_ip_detach(tmp_ins_obj,flt_ip)
-    
-    delete_q = "update nova.instances set deleted=1 where uuid='{0}' and  host = '{1}';".format(uuid,host_name)
-    print(delete_q+"\n")
-    #delete_cur = con.cursor()
-    #delete_cur.execute(delete_q)
-    
     
 
 
 
 ### Cinder Functions ###
     
-def volume_detach(volume_id):
-    cinder.volumes.detach(volume_id)
+def volume_detach(vol_dict):
+    for vol_id in vol_dict:
+        print("Fake Volume detach",vol_id,"\n")
+        cinder.volumes.detach(vol_id)
 
 
-def volume_attach(volume_id,inst_id):
-    cinder.volumes.attach(volume_id,inst_id,"/dev/vdc","rw")
+def volume_attach(vol_dict,inst_id):
+    for vol_id in vol_dict:
+        print("Fake Volume attach",vol_id,"\n",inst_id,vol_dict[vol_id])
+        cinder.volumes.attach(vol_id,inst_id,vol_dict[vol_id],"rw")
 
 
 ### FLOATING IP ###
 
-def flt_ip_detach(instance_obj,flt_ip):
-    instance_obj.remove_floating_ip(flt_ip)
+def flt_ip_detach(instance_obj,flt_ip_list):
+    for flt_ip in flt_ip_list:
+        print("Fake IP detach",instance_obj.name,"\n",flt_ip,"\n")    
+        instance_obj.remove_floating_ip(flt_ip)
 
-def flt_ip_attach(instance_obj,flt_ip):
-    instance_obj.add_floating_ip(flt_ip)
+def flt_ip_attach(instance_obj,flt_ip_list):
+    for flt_ip in flt_ip_list:
+        print("Fake IP attach",instance_obj.name,"\n",flt_ip,"\n")    
+        instance_obj.add_floating_ip(flt_ip)
     
 
 
 
-
-while True:
-    dhosts = down_host()
-    print len(dhosts)
-    if len(dhosts) > 0 :
+def main():
+    while True:
+        dhosts = down_hosts()
         uhosts = active_hosts()
-        if len(dhosts) <= len(uhosts)+len(dhosts)-1:
-            for dhost in dhosts:
-                for instance_obj in list_instances(dhost.host):
-                    inst_dict = parse_inst_details(instance_obj)
-                    delete_instance_mysql(inst_dict['inst_id'],inst_dict['volume_id'],dhost.host)
-                    create_instance(inst_dict['name'],inst_dict['volume_id'],inst_dict['flt_ip'])
-                    time.sleep(5)
-                host_delete(dhost)        
-                print "Evacuation and host disabled"
+        print len(dhosts)
+        if len(dhosts) > 0 :
+            if len(dhosts) <= len(uhosts)+len(dhosts)-1:
+                for dhost in dhosts:
+                    for instance_obj in list_instances(dhost.host):
+                        inst_dict = parse_inst_details(instance_obj)
+                        try:
+                            delete_instance_mysql(inst_dict['inst_id'],inst_dict['vol_dict'],dhost.host,inst_dict['flt_ip_list'])
+                            create_instance_mysql(inst_dict['name'],inst_dict['vol_dict'],inst_dict['flt_ip_list'])
+                        except:
+                            pass
+                        time.sleep(5)
+                    host_delete(dhost)        
+                    print "Evacuation and host disabled"
+            else:
+                print "Not Enough Hosts Found "
         else:
-            print "Not Enought Hosts Found "
-    else:
-        print "Running Normally"
-    time.sleep(120)
+            print "Running Normally"
+        time.sleep(120)
 
